79b43ea2b9efe030fbd321699fb94ef7213400cf
RuslanGon <ruslan441006@gmail.com>
2024-01-04 16:52:52 +0200

    dom

---
+++ js/gallery.js
@@ -0,0 +1,124 @@
+const images = [
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+      description: "Hokkaido Flower",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+      description: "Container Haulage Freight",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+      description: "Aerial Beach View",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+      description: "Flower Blooms",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+      description: "Alpine Mountains",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+      description: "Mountain Lake Sailing",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+      description: "Alpine Spring Meadows",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+      description: "Nature Landscape",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+      description: "Lighthouse Coast Sea",
+    },
+  ];
+
+  const galleryList = document.querySelector('.gallery');
+  function galleryTemp(img) {
+    return `<li class="gallery-item">
+    <a class="gallery-link" href="${img.original}" onclick="event.preventDefault()">
+      <img
+        class="gallery-image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+  </li>`
+  }
+
+  function renderGallery() {
+    const markup = images.map(galleryTemp).join('');
+    galleryList.insertAdjacentHTML('beforeend', markup);
+  }
+  renderGallery();
+
+galleryList.addEventListener("click", function (event) {
+    console.log(event.target);
+})
+  
+
+//=============================//
+document.addEventListener("DOMContentLoaded", function() {
+  // Отримати елементи
+  const imageLink = document.querySelector('.gallery-link');
+  const largeImage = document.querySelector('.gallery-image');
+
+  // Додати подію кліку на посилання
+  imageLink.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Отримати шлях до великого зображення з data-атрибуту
+    const source = largeImage.getAttribute("data-source");
+
+    // Встановити посилання на велике зображення в href
+    imageLink.href = source;
+
+    // Додати атрибут src на елемент img
+    largeImage.src = source;
+  });
+});
+
+
+
+//=========перевіряємо цільовий елемент події click===========================//
+function selectImage(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return; // користувач клікнув між кнопками
+  }
+
+ // const selectedImage = event.target.dataset.color; // користувач клікнув на кнопку і ми маємо доступ до її атрибутів
+}
+
+//==================================================//
